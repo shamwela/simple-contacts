413552ea7ad34ae6a623625211803e1e40be44cf
Sha Mwe La <shamwela2002@gmail.com>
2021-06-27 15:53:10 +0630

    Fix wrong input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import CreateContactButton from './components/createContactButton';
 import CreateContactForm from './components/createContactForm';
