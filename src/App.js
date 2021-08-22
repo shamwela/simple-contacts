acf0670b698febad39cc44253d2dcaef8a0b15e9
Sha Mwe La <shamwela2002@gmail.com>
2021-08-22 14:53:25 +0630

    Implement user authentication using Firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import _ from 'lodash';
-import Search from './components/common/search';
-import CreateContactButton from './components/createContactButton';
-import CreateContactForm from './components/createContactForm';
-import ContactDetails from './components/contactDetails';
-import Contacts from './components/contacts';
-import './App.css';
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import _ from 'lodash'
+import withFirebaseAuth from 'react-with-firebase-auth'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from './firebaseConfig'
+import Search from './components/common/search'
+import CreateContactButton from './components/createContactButton'
+import CreateContactForm from './components/createContactForm'
+import ContactDetails from './components/contactDetails'
+import Contacts from './components/contacts'
+import './App.css'
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseAppAuth = firebaseApp.auth()
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+}
 
 class App extends Component {
   state = {
@@ -34,99 +44,113 @@ class App extends Component {
     openedContact: {},
     isCreateContactFormOpened: false,
     isContactDetailsOpened: false,
-  };
+  }
 
   handleSearch = (search) => {
-    this.setState({ search });
-  };
+    this.setState({ search })
+  }
 
   handleSaveContact = (contact) => {
-    let contacts = [...this.state.contacts];
+    let contacts = [...this.state.contacts]
 
     // delete the old contact if it already exists
-    contacts = contacts.filter((item) => item.id !== contact.id);
+    contacts = contacts.filter((item) => item.id !== contact.id)
 
     // add the new contact to the contacts
-    contacts.push(contact);
+    contacts.push(contact)
 
-    this.setState({ contacts, isCreateContactFormOpened: false });
+    this.setState({ contacts, isCreateContactFormOpened: false })
 
-    this.handleOpenContactDetails(contact);
-  };
+    this.handleOpenContactDetails(contact)
+  }
 
   handleContactFormClose = () => {
-    this.setState({ isCreateContactFormOpened: false });
-  };
+    this.setState({ isCreateContactFormOpened: false })
+  }
 
   handleOpenContactDetails = (contact) => {
-    this.setState({ openedContact: contact, isContactDetailsOpened: true });
-  };
+    this.setState({ openedContact: contact, isContactDetailsOpened: true })
+  }
 
   handleDeleteContact = (contact) => {
-    let contacts = [...this.state.contacts];
-    contacts = contacts.filter((item) => item.id !== contact.id);
-    this.setState({ contacts, isContactDetailsOpened: false });
-  };
+    let contacts = [...this.state.contacts]
+    contacts = contacts.filter((item) => item.id !== contact.id)
+    this.setState({ contacts, isContactDetailsOpened: false })
+  }
 
   render() {
+    const { user, signOut, signInWithGoogle } = this.props
+
     const {
       contacts: allContacts,
       search,
       openedContact,
       isCreateContactFormOpened,
       isContactDetailsOpened,
-    } = this.state;
+    } = this.state
 
     const sortedContacts = _.sortBy(allContacts, [
       (contact) => contact.name.toLocaleLowerCase(),
-    ]);
+    ])
 
-    let contacts = sortedContacts;
+    let contacts = sortedContacts
     if (search) {
       contacts = sortedContacts.filter((contact) =>
         contact.name.toLowerCase().includes(search.toLowerCase())
-      );
+      )
     } else {
-      contacts = sortedContacts;
+      contacts = sortedContacts
     }
 
     return (
-      <div id="app">
-        <h1 id="app-title">Simple Contacts</h1>
+      <>
+        {user ? (
+          <div id='app'>
+            <h1 id='app-title'>Simple Contacts</h1>
 
-        <Search search={search} onSearch={this.handleSearch} />
+            <Search search={search} onSearch={this.handleSearch} />
 
-        <CreateContactButton
-          onCreateContact={() =>
-            this.setState({ isCreateContactFormOpened: true })
-          }
-        />
-        {isCreateContactFormOpened && (
-          <CreateContactForm
-            onSaveContact={(contact) => this.handleSaveContact(contact)}
-            onContactFormClose={this.handleContactFormClose}
-          />
+            <CreateContactButton
+              onCreateContact={() =>
+                this.setState({ isCreateContactFormOpened: true })
+              }
+            />
+            {isCreateContactFormOpened && (
+              <CreateContactForm
+                onSaveContact={(contact) => this.handleSaveContact(contact)}
+                onContactFormClose={this.handleContactFormClose}
+              />
+            )}
+
+            <Contacts
+              contacts={contacts}
+              onOpen={(contact) => this.handleOpenContactDetails(contact)}
+            />
+
+            {isContactDetailsOpened && (
+              <ContactDetails
+                contact={openedContact}
+                onDeleteContact={this.handleDeleteContact}
+                onContactDetailsClose={() =>
+                  this.setState({ isContactDetailsOpened: false })
+                }
+                onSaveContact={(contact) => this.handleSaveContact(contact)}
+                onContactFormClose={this.handleContactFormClose}
+              />
+            )}
+          </div>
+        ) : (
+          <>
+            <button onClick={signInWithGoogle}>Continue with Google</button>
+            <small>Your data won't be shared with anyone.</small>
+          </>
         )}
-
-        <Contacts
-          contacts={contacts}
-          onOpen={(contact) => this.handleOpenContactDetails(contact)}
-        />
-
-        {isContactDetailsOpened && (
-          <ContactDetails
-            contact={openedContact}
-            onDeleteContact={this.handleDeleteContact}
-            onContactDetailsClose={() =>
-              this.setState({ isContactDetailsOpened: false })
-            }
-            onSaveContact={(contact) => this.handleSaveContact(contact)}
-            onContactFormClose={this.handleContactFormClose}
-          />
-        )}
-      </div>
-    );
+      </>
+    )
   }
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App)
