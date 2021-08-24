87f6acbe4eb9db26131894e9dfcbf4df267a7b0d
Sha Mwe La <shamwela2002@gmail.com>
2021-08-24 16:56:57 +0630

    Implement creating contact

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import Joi from 'joi';
-import SingleInput from './singleInput';
-import MultipleInput from './multipleInput';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Component } from 'react'
+import Joi from 'joi'
+import SingleInput from './singleInput'
+import MultipleInput from './multipleInput'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 class Form extends Component {
   state = {
     data: {},
     errors: {},
-  };
+  }
 
   schema = Joi.object({
     id: Joi.string().required(),
@@ -24,77 +26,89 @@ class Form extends Component {
     phones: Joi.array().items(
       Joi.string().allow(null, '').max(20).label('Phone')
     ),
-  });
+  })
 
   validateField = ({ name, value }) => {
-    const schemaForField = Joi.object({ [name]: this.schema.extract(name) });
+    const schemaForField = Joi.object({ [name]: this.schema.extract(name) })
     // for example => {name: Joi.string().label('Name')}
 
-    let fieldToValidate = {};
+    let fieldToValidate = {}
     if (name === 'name') {
-      fieldToValidate = { [name]: value }; // for example => {name: 'John Smith'}
+      fieldToValidate = { [name]: value } // for example => {name: 'John Smith'}
     } else {
-      fieldToValidate = { [name]: [value] }; // for example => {emails: ['john@gmail.com']}
+      fieldToValidate = { [name]: [value] } // for example => {emails: ['john@gmail.com']}
     }
 
-    const { error } = schemaForField.validate(fieldToValidate);
-    return error ? error.details[0].message : null;
-  };
+    const { error } = schemaForField.validate(fieldToValidate)
+    return error ? error.details[0].message : null
+  }
 
   validateForm = () => {
-    const options = { abortEarly: false };
-    const result = this.schema.validate(this.state.data, options);
-    if (!result.error) return null;
+    const options = { abortEarly: false }
+    const result = this.schema.validate(this.state.data, options)
+    if (!result.error) return null
 
-    const errors = {};
-    for (let item of result.error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
+    const errors = {}
+    for (let item of result.error.details) errors[item.path[0]] = item.message
+    return errors
+  }
 
   handleChange = ({ currentTarget: input }, index) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateField(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
+    const errors = { ...this.state.errors }
+    const errorMessage = this.validateField(input)
+    if (errorMessage) errors[input.name] = errorMessage
+    else delete errors[input.name]
 
-    const data = { ...this.state.data };
-    const { name, value } = input;
+    const data = { ...this.state.data }
+    const { name, value } = input
     // for name
     if (typeof data[name] === 'string') {
-      data.name = value;
+      data.name = value
     } else {
       // for emails and phones
-      data[name][index] = value;
+      data[name][index] = value
     }
-    this.setState({ data, errors });
-  };
+    this.setState({ data, errors })
+  }
 
   handleInputDelete = (e, index) => {
-    e.preventDefault();
-    const data = { ...this.state.data };
-    const name = e.currentTarget.name;
-    data[name].splice(index, 1);
-    this.setState({ data });
-  };
+    e.preventDefault()
+    const data = { ...this.state.data }
+    const name = e.currentTarget.name
+    data[name].splice(index, 1)
+    this.setState({ data })
+  }
 
   handleAddNewInput = (e) => {
-    e.preventDefault();
-    const data = { ...this.state.data };
-    const name = e.currentTarget.name;
-    data[name].push('');
-    this.setState({ data });
-  };
+    e.preventDefault()
+    const data = { ...this.state.data }
+    const name = e.currentTarget.name
+    data[name].push('')
+    this.setState({ data })
+  }
 
   handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validateForm();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-    this.props.onSaveContact(this.state.data);
-  };
+    e.preventDefault()
+    const errors = this.validateForm()
+    this.setState({ errors: errors || {} })
+    if (errors) return
+
+    const userID = firebase.auth().currentUser.uid
+    const { id: contactID, name, emails, phones } = this.state.data
+    firebase
+      .firestore()
+      .collection(userID)
+      .doc(contactID)
+      .set({
+        name,
+        emails,
+        phones,
+      })
+      .catch((error) => console.log(error))
+  }
 
   renderSingleInput = (name, label, type = 'text') => {
-    const { data, errors } = this.state;
+    const { data, errors } = this.state
 
     return (
       <SingleInput
@@ -105,11 +119,11 @@ class Form extends Component {
         onChange={this.handleChange}
         error={errors[name]}
       />
-    );
-  };
+    )
+  }
 
   renderMultipleInput = (name, label, type = 'text') => {
-    const { data, errors } = this.state;
+    const { data, errors } = this.state
 
     return (
       <MultipleInput
@@ -122,27 +136,27 @@ class Form extends Component {
         onAddNewInput={this.handleAddNewInput}
         error={errors[name]}
       />
-    );
-  };
+    )
+  }
 
   renderSubmitButton = () => (
     <button
       disabled={this.validateForm()}
-      type="submit"
-      className="btn btn-primary"
+      type='submit'
+      className='btn btn-primary'
     >
-      <FontAwesomeIcon icon="save" /> Save
+      <FontAwesomeIcon icon='save' /> Save
     </button>
-  );
+  )
 
   renderCancelButton = () => (
     <button
       onClick={(e) => this.props.onContactFormClose(e.preventDefault())}
-      className="btn btn-secondary"
+      className='btn btn-secondary'
     >
-      <FontAwesomeIcon icon="window-close" /> Cancel
+      <FontAwesomeIcon icon='window-close' /> Cancel
     </button>
-  );
+  )
 }
 
-export default Form;
+export default Form
